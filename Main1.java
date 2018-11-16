b23d673fd8277acbf670e89004ce7afa06e00bbf
KarolCzechowicz <44444298+KarolCzechowicz@users.noreply.github.com>
2018-11-16 12:24:33 +0100

    initial commit

---
+++ Main1.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        System.out.println("Pole prostokąta to: " + rectangle(5,8));
+
+    }
+    static int rectangle(int a, int b) {
+        int temp = a * b;
+        return temp;
+    }
+}
