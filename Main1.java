79a9067e61b01b7a085bf3522cd052c07ecc6d82
KarolCzechowicz <44444298+KarolCzechowicz@users.noreply.github.com>
2018-11-16 13:38:22 +0100

    Zrobione zad. 1

--- Main1.java
+++ Main1.java
@@ -1,13 +1,28 @@
-package exam;
+package pl.coderslab;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main1 {
-
     public static void main(String[] args) {
-        System.out.println("Pole prostokąta to: " + rectangle(5,8));
+        numberGues();
 
     }
-    static int rectangle(int a, int b) {
-        int temp = a * b;
-        return temp;
+    static void numberGues () {
+        Random random = new Random();
+        int ran = random.nextInt(101 - 1) - 1;
+        Scanner scan = new Scanner(System.in);
+        int input = 0;
+        do {
+            System.out.println("Zgadnij liczbę");
+             input = scan.nextInt();
+            if (input < ran) {
+                System.out.println("Za mało");
+            } else if (input > ran) {
+                System.out.println("Za duzo");
+            }
+        } while (input != ran); {
+        }
+        System.out.println("Zgadłeś");
     }
 }
